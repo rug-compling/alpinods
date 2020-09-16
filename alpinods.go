8752fd3d103b3d2c56edb5f93cc69b37823d846d
Peter Kleiweg <p.c.j.kleiweg@rug.nl>
2020-09-16 15:14:15 +0200

    alpino_ds versie 1.12

--- alpinods.go
+++ alpinods.go
@@ -80,56 +80,50 @@ type Conllu struct {
 // The NodeAttributes type encodes the attributes for
 // `/alpino_ds//node`.
 type NodeAttributes struct {
-	Aform       string `xml:"aform,attr,omitempty"`
-	Begin       int    `xml:"begin,attr"`
-	Buiging     string `xml:"buiging,attr,omitempty"`
-	Case        string `xml:"case,attr,omitempty"`
-	Cat         string `xml:"cat,attr,omitempty"`
-	Comparative string `xml:"comparative,attr,omitempty"`
-	Conjtype    string `xml:"conjtype,attr,omitempty"`
-	Def         string `xml:"def,attr,omitempty"`
-	Dial        string `xml:"dial,attr,omitempty"`
-	Dscmanual   string `xml:"dscmanual,attr,omitempty"`
-	Dscsense    string `xml:"dscsense,attr,omitempty"`
-	End         int    `xml:"end,attr"`
-	Frame       string `xml:"frame,attr,omitempty"`
-	Gen         string `xml:"gen,attr,omitempty"`
-	Genus       string `xml:"genus,attr,omitempty"`
-	Getal       string `xml:"getal,attr,omitempty"`
-	GetalN      string `xml:"getal-n,attr,omitempty"`
-	Graad       string `xml:"graad,attr,omitempty"`
-	His         string `xml:"his,attr,omitempty"`
-	His1        string `xml:"his_1,attr,omitempty"`
-	His2        string `xml:"his_2,attr,omitempty"`
-	His11       string `xml:"his_1_1,attr,omitempty"`
-	His12       string `xml:"his_1_2,attr,omitempty"`
-	His21       string `xml:"his_2_1,attr,omitempty"`
-	His22       string `xml:"his_2_2,attr,omitempty"`
-	His111      string `xml:"his_1_1_1,attr,omitempty"`
-	His112      string `xml:"his_1_1_2,attr,omitempty"`
-	His121      string `xml:"his_1_2_1,attr,omitempty"`
-	His122      string `xml:"his_1_2_2,attr,omitempty"`
-	His211      string `xml:"his_2_1_1,attr,omitempty"`
-	His212      string `xml:"his_2_1_2,attr,omitempty"`
-	His221      string `xml:"his_2_2_1,attr,omitempty"`
-	His222      string `xml:"his_2_2_2,attr,omitempty"`
-	ID          int    `xml:"id,attr"`
-	Iets        string `xml:"iets,attr,omitempty"`
-	Index       int    `xml:"index,attr,omitempty"`
-	Infl        string `xml:"infl,attr,omitempty"`
-	Lcat        string `xml:"lcat,attr,omitempty"`
-	Lemma       string `xml:"lemma,attr,omitempty"`
-	Lwtype      string `xml:"lwtype,attr,omitempty"`
-	MwuRoot     string `xml:"mwu_root,attr,omitempty"`
-	MwuSense    string `xml:"mwu_sense,attr,omitempty"`
-	Naamval     string `xml:"naamval,attr,omitempty"`
-	Neclass     string `xml:"neclass,attr,omitempty"`
-
-	Ne      string `xml:"ne,attr,omitempty"`
-	NeVal   string `xml:"ne_val,attr,omitempty"`
-	NeBegin string `xml:"ne_begin,attr,omitempty"`
-	NeEnd   string `xml:"ne_end,attr,omitempty"`
-
+	Aform        string `xml:"aform,attr,omitempty"`
+	Begin        int    `xml:"begin,attr"`
+	Buiging      string `xml:"buiging,attr,omitempty"`
+	Case         string `xml:"case,attr,omitempty"`
+	Cat          string `xml:"cat,attr,omitempty"`
+	Comparative  string `xml:"comparative,attr,omitempty"`
+	Conjtype     string `xml:"conjtype,attr,omitempty"`
+	Def          string `xml:"def,attr,omitempty"`
+	Dial         string `xml:"dial,attr,omitempty"`
+	Dscmanual    string `xml:"dscmanual,attr,omitempty"`
+	Dscsense     string `xml:"dscsense,attr,omitempty"`
+	End          int    `xml:"end,attr"`
+	Frame        string `xml:"frame,attr,omitempty"`
+	Gen          string `xml:"gen,attr,omitempty"`
+	Genus        string `xml:"genus,attr,omitempty"`
+	Getal        string `xml:"getal,attr,omitempty"`
+	GetalN       string `xml:"getal-n,attr,omitempty"`
+	Graad        string `xml:"graad,attr,omitempty"`
+	His          string `xml:"his,attr,omitempty"`
+	His1         string `xml:"his_1,attr,omitempty"`
+	His2         string `xml:"his_2,attr,omitempty"`
+	His11        string `xml:"his_1_1,attr,omitempty"`
+	His12        string `xml:"his_1_2,attr,omitempty"`
+	His21        string `xml:"his_2_1,attr,omitempty"`
+	His22        string `xml:"his_2_2,attr,omitempty"`
+	His111       string `xml:"his_1_1_1,attr,omitempty"`
+	His112       string `xml:"his_1_1_2,attr,omitempty"`
+	His121       string `xml:"his_1_2_1,attr,omitempty"`
+	His122       string `xml:"his_1_2_2,attr,omitempty"`
+	His211       string `xml:"his_2_1_1,attr,omitempty"`
+	His212       string `xml:"his_2_1_2,attr,omitempty"`
+	His221       string `xml:"his_2_2_1,attr,omitempty"`
+	His222       string `xml:"his_2_2_2,attr,omitempty"`
+	ID           int    `xml:"id,attr"`
+	Iets         string `xml:"iets,attr,omitempty"`
+	Index        int    `xml:"index,attr,omitempty"`
+	Infl         string `xml:"infl,attr,omitempty"`
+	Lcat         string `xml:"lcat,attr,omitempty"`
+	Lemma        string `xml:"lemma,attr,omitempty"`
+	Lwtype       string `xml:"lwtype,attr,omitempty"`
+	MwuRoot      string `xml:"mwu_root,attr,omitempty"`
+	MwuSense     string `xml:"mwu_sense,attr,omitempty"`
+	Naamval      string `xml:"naamval,attr,omitempty"`
+	Neclass      string `xml:"neclass,attr,omitempty"`
 	Npagr        string `xml:"npagr,attr,omitempty"`
 	Ntype        string `xml:"ntype,attr,omitempty"`
 	Num          string `xml:"num,attr,omitempty"`
@@ -152,6 +146,10 @@ type NodeAttributes struct {
 	Root         string `xml:"root,attr,omitempty"`
 	Sc           string `xml:"sc,attr,omitempty"`
 	Sense        string `xml:"sense,attr,omitempty"`
+	SonarNe      string `xml:"sonar_ne,attr,omitempty"`
+	SonarNeClass string `xml:"sonar_ne_class,attr,omitempty"`
+	SonarNeBegin string `xml:"sonar_ne_begin,attr,omitempty"`
+	SonarNeEnd   string `xml:"sonar_ne_end,attr,omitempty"`
 	Special      string `xml:"special,attr,omitempty"`
 	Spectype     string `xml:"spectype,attr,omitempty"`
 	Status       string `xml:"status,attr,omitempty"`
